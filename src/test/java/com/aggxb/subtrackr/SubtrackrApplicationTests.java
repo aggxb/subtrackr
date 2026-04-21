4ab986713ad3013f361b973028cbebd7613f08fd
Gabriel Augusto <ag71gabriel@gmail.com>
2026-04-21 12:05:35 -0300

    first commit

---
+++ src/test/java/com/aggxb/subtrackr/SubtrackrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aggxb.subtrackr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubtrackrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
