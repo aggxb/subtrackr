8e701420f69159627f26f304db943de64f0ec713
Gabriel Augusto <ag71gabriel@gmail.com>
2026-04-21 12:47:30 -0300

    feat: implement subscription domain and repository structure

---
+++ src/main/java/com/aggxb/subtrackr/enums/BillingCycle.java
@@ -0,0 +1,5 @@
+package com.aggxb.subtrackr.enums;
+
+public enum BillingCycle {
+    MONTHLY, YEARLY
+}
---
+++ src/main/java/com/aggxb/subtrackr/enums/SubscriptionStatus.java
@@ -0,0 +1,5 @@
+package com.aggxb.subtrackr.enums;
+
+public enum SubscriptionStatus {
+    ACTIVE, CANCELED
+}
